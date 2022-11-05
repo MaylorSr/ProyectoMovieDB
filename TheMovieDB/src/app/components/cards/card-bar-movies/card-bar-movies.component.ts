27c1668dfed12b23cb19717b62991348ef879682
jethrojonson <biojpg@gmail.com>
2022-11-05 13:24:18 +0100

    Se termina la vista de la lista de peliculas populares

--- TheMovieDB/src/app/components/cards/card-bar-movies/card-bar-movies.component.ts
+++ TheMovieDB/src/app/components/cards/card-bar-movies/card-bar-movies.component.ts
@@ -13,6 +13,11 @@ export class CardBarMoviesComponent implements OnInit {
   numPagesTotal = 0;
   page = 1;
   pageActual : number =this.page;
+  voteAvg : number = 0;
+  voteAvgStr : String = "";
+  votesColor: String = "";
+  votesBgColor: String = "";
+
   listMovies : PopularMovie[] = [];
   @Input() movie : PopularMovie = {} as PopularMovie;
 
@@ -25,5 +30,29 @@ export class CardBarMoviesComponent implements OnInit {
     return `${environment.api_base_img}${movie.poster_path}`;
   }
 
+  public getVoteAvg(movie : PopularMovie){
+   this.voteAvg = movie.vote_average*10;
+   this.voteAvgStr = "width: "+this.voteAvg + "%"
+  }
 
+  public setVotesColor(movie: PopularMovie){
+    this.getVoteAvg(movie)
+    if(this.voteAvg >=75){
+      this.votesBgColor = "overflow-hidden h-2 text-xs flex rounded bg-emerald-200"
+      this.votesColor = "shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500"
+    }
+    else if(this.voteAvg>=60){
+      this.votesBgColor = "overflow-hidden h-2 text-xs flex rounded bg-blueGray-200"
+      this.votesColor = "shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500"
+    }
+    else if(this.voteAvg>=50){
+      this.votesBgColor = "overflow-hidden h-2 text-xs flex rounded bg-orange-200"
+      this.votesColor = "shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-yellow-500"
+    }
+    else{
+      this.votesBgColor = "overflow-hidden h-2 text-xs flex rounded bg-red-200"
+      this.votesColor = "shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
+    }
+    return this.votesBgColor;
+  }
 }
